8bc4898424a635ce868b3357fc289794e438b867
Ricardo Chaves <ricardochaves@ua.pt>
2025-07-25 22:56:15 +0100

    deploy: add basic logging

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 
+DEPLOY_LOG="/tmp/deploy_personal_page_$(date +%s).log"
 PAGE_REPO="/home/worker/personal-page"
 
 cd "$PAGE_REPO" || exit 1
 
-# Pull latest changes
+echo "Pull latest changes" >> $DEPLOY_LOG
 git pull origin master
 
-# Sync to web root
+echo "Syncing to webroot" >> $DEPLOY_LOG
 rsync -a --delete --exclude=".git" "${PAGE_REPO}/" /var/www/personal/
+echo "Synced!" >> $DEPLOY_LOG
